e076c3591342b88abb9d3b81065127f31ccdf51d
Marco_Sanint <marcosanint8@gmail.com>
2024-11-07 21:16:06 -0500

    Proyecto creado con backend en spring boot

---
+++ ConciertosYa/src/main/java/com/software2/ConciertosYa/ConciertosYaApplication.java
@@ -0,0 +1,13 @@
+package com.software2.ConciertosYa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConciertosYaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConciertosYaApplication.class, args);
+	}
+
+}
---
+++ ConciertosYa/src/test/java/com/software2/ConciertosYa/ConciertosYaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software2.ConciertosYa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConciertosYaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
